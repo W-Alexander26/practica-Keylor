be2f0032a254d07a7482fcfb1d0b2e7c87a18f62
W-Alexander <alexanderaguilar726@gmail.com>
2024-05-29 08:15:16 -0600

    Prueba

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//Practica:
+//recorrer el array
+//por cada persona crear un objeto
+//crear un método Saludar, que diga Hola me llamo nombre + apellido
+//guardar los objetos en un nuevo array
+//recorrer el array de objetos y llamar el metodo saludar por cada uno de hecho
+//output esperado:
+//Hola me llamo John Doe
+//...
+
+/*arrayNombres = [
+    ["John", "Doe"],
+    ["Jane", "Smith"],
+    ["Alice", "Johnson"],
+    ["Bob", "Brown"],
+    ["Eve", "Davis"]
+];*/
